70749a0851e8729eb06f0a4db35386a41d3fcabd
pe-bconf <77265363+pe-bconf@users.noreply.github.com>
2026-02-20 15:14:20 +0900

    Feature: 챕터5(5.1.3)
    - 예제 추가

---
+++ Vol1-31/Ch5/5.1.3/src/main/kotlin/toby/spring/user/service/UserService.kt
@@ -0,0 +1,38 @@
+package toby.spring.user.service
+
+import org.eclipse.core.internal.runtime.PerformanceStatsProcessor.changed
+import toby.spring.user.dao.UserDao
+import toby.spring.user.domain.Level
+import toby.spring.user.domain.User
+
+class UserService {
+    private lateinit var userDao: UserDao
+
+    fun setUserDao(userDao: UserDao) {
+        this.userDao = userDao
+    }
+
+    fun updateLevels() {
+        val users: List<User> = userDao.getAll()
+
+        for (user in users) {
+            var changed: Boolean? = null
+
+            if (user.level == Level.BASIC && user.login!! >= 50) {
+                user.level = Level.SILVER
+                changed = true
+            } else if (user.level == Level.SILVER && user.recommend!! >= 30) {
+                user.level = Level.GOLD
+                changed = true
+            } else if (user.level == Level.GOLD) {
+                changed = false
+            } else {
+                changed = false
+            }
+
+            if (changed) {
+                this.userDao.update(user)
+            }
+        }
+    }
+}
