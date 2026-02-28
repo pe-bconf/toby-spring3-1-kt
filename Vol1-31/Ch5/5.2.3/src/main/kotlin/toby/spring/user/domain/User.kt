47e2f77f39e86a8dad26daff3d761dd60acfc45d
pe-bconf <77265363+pe-bconf@users.noreply.github.com>
2026-03-01 01:39:55 +0900

    Feature: 챕터5(5.2.3)
    - 예제 추가

---
+++ Vol1-31/Ch5/5.2.3/src/main/kotlin/toby/spring/user/domain/User.kt
@@ -0,0 +1,15 @@
+package toby.spring.user.domain
+
+data class User(
+    var id: String?, var name: String?, var password: String?,
+    var level: Level?, var login: Int?, var recommend: Int?) {
+    constructor(): this(null, null, null, null, null, null) {}
+    fun upgradeLevel() {
+        val nextLevel =
+            (this.level
+                ?: throw IllegalStateException("레벨이 설정되지 않은 사용자입니다.")).nextLevel()
+                ?: throw IllegalStateException("${this.level}은 업그레이드가 불가능합니다.")
+
+        this.level = nextLevel
+    }
+}
